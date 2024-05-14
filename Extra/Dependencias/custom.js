88d69027cc370fb79036be4a5116b54e89ae2380
Jaime Aquino Martinez <jhonesbortoni@gmail.com>
2024-05-13 19:10:20 -0500

    Normalizacion de primer Semestre de WEB

--- Extra/Dependencias/custom.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-	document.querySelector('#btn-print').addEventListener('click', function () {
-		// window.print();
-
-		let wspFrame = document.getElementById('frame').contentWindow;
-		wspFrame.focus();
-		wspFrame.print();
-	});
-});
--- Graficacion 6to Semestre/Ejercicio1.py
+++
@@ -1,4 +0,0 @@
-x = []
-for i in range(10):
-    x.append(i*2)
-    print(x)
